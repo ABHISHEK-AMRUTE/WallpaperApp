f3c4af45ee7771fc32ebb38879d522a6d76af445
bigblackclock <abdlhadi1999apdo@gmail.com>
2020-10-18 17:29:55 +0200

    added download functionaltiy and set wallpaper

--- lib/views/preview_image.dart
+++ lib/views/preview_image.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:wallpaper/wallpaper.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:image_downloader/image_downloader.dart';
+import 'package:wallpaper_manager/wallpaper_manager.dart';
 
 class PreviewInDetail extends StatefulWidget {
   int width, height, photographerId;
@@ -42,126 +44,176 @@ class PreviewInDetail extends StatefulWidget {
 }
 
 class _PreviewInDetailState extends State<PreviewInDetail> {
-  void setWallpaper() async {
-    var progressString = Wallpaper.ImageDownloadProgress(widget.urlPlacehoder);
-    progressString.listen((data) {
-      print("DataReceived: " + data);
-    }, onDone: () async {
-      var width = MediaQuery.of(context).size.width;
-      var height = MediaQuery.of(context).size.height;
-      var home = await Wallpaper.homeScreen();
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  void setWallpaper(int wallpaperLocation) async {
+    try {
+      var file =
+          await DefaultCacheManager().getSingleFile(widget.urlPlacehoder);
+      await WallpaperManager.setWallpaperFromFile(file.path, wallpaperLocation);
+      Navigator.pop(context);
 
-      print("Task Done");
-    }, onError: (error) {
-      print("Some Error");
-    });
+      _scaffoldKey.currentState.showSnackBar(SnackBar(
+        content: Text('Wallpaper has been set'),
+        duration: Duration(seconds: 2),
+      ));
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  void downloadWallpaper() async {
+    try {
+      var imageId = await ImageDownloader.downloadImage(widget.urlPlacehoder);
+      if (imageId == null) {
+        return;
+      }
+
+      _scaffoldKey.currentState.showSnackBar(SnackBar(
+        content: Text('Wallpaper has been Donwloaded'),
+        duration: Duration(seconds: 2),
+      ));
+    } catch (error) {
+      print(error);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Stack(children: [
-       
-        InteractiveViewer(
-          panEnabled: true,
-          minScale: 0.5,
-          maxScale: 50,
-                  child: CachedNetworkImage(
-              // fit: BoxFit.fill,
-              height: MediaQuery.of(context).size.height,
-              placeholder: (context, url) =>
-                  Image.asset("assets/images/sample.gif", height: 100),
-              imageUrl: widget.urlPlacehoder),
-        ),
-        Align(
-          alignment: Alignment.topRight,
-          child: Padding(
-            padding: EdgeInsets.all(10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                DropdownButton(
-                    value: widget.valueDropDown,
-                    items: [
-                      DropdownMenuItem(
-                        child: Text("Original"),
-                        value: 1,
-                      ),
-                      DropdownMenuItem(
-                        child: Text("Large 2x"),
-                        value: 2,
-                      ),
-                      DropdownMenuItem(
-                        child: Text("Large"),
-                        value: 3,
-                      ),
-                      DropdownMenuItem(
-                        child: Text("Medium"),
-                        value: 4,
-                      ),
-                      DropdownMenuItem(
-                        child: Text("Small"),
-                        value: 5,
-                      ),
-                      DropdownMenuItem(
-                        child: Text("Portrait"),
-                        value: 6,
-                      ),
-                      DropdownMenuItem(
-                        child: Text("Landscape"),
-                        value: 7,
-                      ),
-                      DropdownMenuItem(
-                        child: Text("Tiny"),
-                        value: 8,
-                      ),
-                    ],
-                    onChanged: (value) {
-                      widget.valueDropDown = value;
-                      switch (value) {
-                        case 1:
-                          widget.urlPlacehoder = widget.original;
-                          break;
-                        case 2:
-                          widget.urlPlacehoder = widget.large2x;
-                          break;
-                        case 3:
-                          widget.urlPlacehoder = widget.large;
-                          break;
-                        case 4:
-                          widget.urlPlacehoder = widget.medium;
-                          break;
-                        case 5:
-                          widget.urlPlacehoder = widget.small;
-                          break;
-                        case 6:
-                          widget.urlPlacehoder = widget.portrait;
-                          break;
-                        case 7:
-                          widget.urlPlacehoder = widget.landscape;
-                          break;
-                        case 8:
-                          widget.urlPlacehoder = widget.tiny;
-                          break;
-                        default:
-                          widget.urlPlacehoder = widget.portrait;
-                          break;
-                      }
-                      setState(() {});
-                    }),
-                IconButton(
-                    icon: Icon(
-                      Icons.file_download,
-                      color: Colors.blue,
-                    ),
-                    onPressed: () {
-                      setWallpaper();
-                    })
-              ],
-            ),
+      child: Scaffold(
+        key: _scaffoldKey,
+        body: Stack(children: [
+          InteractiveViewer(
+            panEnabled: true,
+            minScale: 0.5,
+            maxScale: 50,
+            child: CachedNetworkImage(
+                // fit: BoxFit.fill,
+                height: MediaQuery.of(context).size.height,
+                placeholder: (context, url) =>
+                    Image.asset("assets/images/sample.gif", height: 100),
+                imageUrl: widget.urlPlacehoder),
           ),
-        )
-      ]),
+          Align(
+            alignment: Alignment.topRight,
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  DropdownButton(
+                      value: widget.valueDropDown,
+                      items: [
+                        DropdownMenuItem(
+                          child: Text("Original"),
+                          value: 1,
+                        ),
+                        DropdownMenuItem(
+                          child: Text("Large 2x"),
+                          value: 2,
+                        ),
+                        DropdownMenuItem(
+                          child: Text("Large"),
+                          value: 3,
+                        ),
+                        DropdownMenuItem(
+                          child: Text("Medium"),
+                          value: 4,
+                        ),
+                        DropdownMenuItem(
+                          child: Text("Small"),
+                          value: 5,
+                        ),
+                        DropdownMenuItem(
+                          child: Text("Portrait"),
+                          value: 6,
+                        ),
+                        DropdownMenuItem(
+                          child: Text("Landscape"),
+                          value: 7,
+                        ),
+                        DropdownMenuItem(
+                          child: Text("Tiny"),
+                          value: 8,
+                        ),
+                      ],
+                      onChanged: (value) {
+                        widget.valueDropDown = value;
+                        switch (value) {
+                          case 1:
+                            widget.urlPlacehoder = widget.original;
+                            break;
+                          case 2:
+                            widget.urlPlacehoder = widget.large2x;
+                            break;
+                          case 3:
+                            widget.urlPlacehoder = widget.large;
+                            break;
+                          case 4:
+                            widget.urlPlacehoder = widget.medium;
+                            break;
+                          case 5:
+                            widget.urlPlacehoder = widget.small;
+                            break;
+                          case 6:
+                            widget.urlPlacehoder = widget.portrait;
+                            break;
+                          case 7:
+                            widget.urlPlacehoder = widget.landscape;
+                            break;
+                          case 8:
+                            widget.urlPlacehoder = widget.tiny;
+                            break;
+                          default:
+                            widget.urlPlacehoder = widget.portrait;
+                            break;
+                        }
+                        setState(() {});
+                      }),
+                  IconButton(
+                      icon: Icon(
+                        Icons.file_download,
+                        color: Colors.blue,
+                      ),
+                      onPressed: () => downloadWallpaper()),
+                  FlatButton(
+                      onPressed: () => showDialog(
+                          context: context,
+                          builder: (_) => new AlertDialog(
+                                title: new Text("Set Wallpaper As"),
+                                content: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    FlatButton(
+                                        onPressed: () => setWallpaper(
+                                            WallpaperManager.HOME_SCREEN),
+                                        child: Text("Home Screen")),
+                                    FlatButton(
+                                        onPressed: () => setWallpaper(
+                                            WallpaperManager.LOCK_SCREEN),
+                                        child: Text("Lock Screen")),
+                                    FlatButton(
+                                        onPressed: () => setWallpaper(
+                                            WallpaperManager.BOTH_SCREENS),
+                                        child: Text("Both Screen")),
+                                  ],
+                                ),
+                                actions: <Widget>[
+                                  FlatButton(
+                                    child: Text('Close me!'),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  )
+                                ],
+                              )),
+                      child: Text("set as wallpaper"))
+                ],
+              ),
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
